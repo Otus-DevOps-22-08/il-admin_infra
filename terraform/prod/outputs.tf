75766b02fbbb2d855c4ebbdee62f03818d72b14c
Ilya Ryzhikov <i.ryzhikov@bi.zone>
2022-11-08 21:55:50 +0300

    il_admin/HW9/modify_terraform_output2

--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -4,3 +4,9 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
 }
+output "internal_ip_address_app" {
+  value = module.app.internal_ip_address_app
+}
+output "internal_ip_address_db" {
+  value = module.db.internal_ip_address_db
+}
